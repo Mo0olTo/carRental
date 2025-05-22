3e399de3a07b84b2b1f080681f65be635d979ef1
Mo0olTo <mostafa.allam.de@gmail.com>
2025-05-22 23:29:54 +0300

    Final

--- src/app/core/Environment/environment.ts
+++ src/app/core/Environment/environment.ts
@@ -1,4 +1,5 @@
 export const environement={
+    production: false,
     baseUrl: "https://freeapi.miniprojectideas.com/api/CarRentalApp/",
-    api: "https://freeapi.miniprojectideas.com/api/CarRentalApp/"
+    api: "/api/CarRentalApp/"
 }
